8850bff63d9705a05b4877075ae6427f0813e4ae
Zander Allen <ztrain01@gmail.com>
2015-01-24 11:39:21 -0700

    name and age

--- practice.js
+++ practice.js
@@ -3,8 +3,10 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
-
+var me = {};
+me.name = "Zander";
+me.age = "25";
+alert(me.name);
 
 
 
